6d95696fcbf3962df63354c2fd8d9838566021fc
amcmullen9291 <amcmullen9291@gmail.com>
2021-08-14 14:50:05 -0400

    safety commit

---
+++ site.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="uft-8">
+    <title>My Page</title>
+    </head>
+    <body>
+        <?php 
+        echo"<h1>My Heading</h1>";
+        echo"<table>
+        <thead>
+        <tr>
+        <td>Venue</td>
+        <td>City</td>
+        <td>Mascot</td>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+        <td>Lambeau</td>
+        <td>Greenbay</td>
+        <td>None</td>
+        </tr>
+        </tbody>
+        </table>";
+
+        echo "<hr/>";
+
+        ?>
+    </body>
+</html>
