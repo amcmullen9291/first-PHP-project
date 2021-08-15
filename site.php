10d167df9648536514b7e1c6c674d1838355d330
amcmullen9291 <amcmullen9291@gmail.com>
2021-08-14 23:15:22 -0400

    boolean if statement assignment. HTML conditional render

--- site.php
+++ site.php
@@ -7,6 +7,23 @@
     </head>
     <body>
         <?php 
+        $Name = "Anthony";
+        $Greeting = "Good Morning";
+        $City = "Green Bay";
+        $number1 = 20;
+        $number2 = "20.00";
+        if($number1 == $number2){
+            $Boolean = "true";
+        }else{
+            $Boolean = "false";
+        }
+        if($number1 === $number2){
+            $Boolean2 = "true";
+        }else{
+            $Boolean2 = "false";
+        }
+
+
         echo"
         <div class='tophalf'><center><h1>Sporting News</h1></center>
         <center><table>
@@ -17,27 +34,44 @@
         <tr>
         <td>Venue</td>
         <td>City</td>
-        <td>Mascot</td>
+        <td>Capacity</td>
         </tr>
         <tbody>
         <tr>
         <td>Lambeau</td>
         <td>Greenbay</td>
-        <td>None</td>
+        <td>80,750</td>
         </tr>
         </tbody>
         </table></center>
         </div>";
 
         echo "<hr/>";
-        echo"<br/><br/>
+        echo"
+        <center id='middle'>$Greeting, $Name!<center>
         <div id='bottom_background'>
         <center><div id='bottom'>
         <div>Bottom Section</div>
         <span id='line2'>(Strikingly similar to HTML)</span>
-        </div></center>
-        </div>"
-
+        <br/>
+        <hr/>
+        <div id='words'>Lorem ipsum dolor sit amet, <b class='city'>$City</b>, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas dui id ornare arcu odio ut. Risus ultricies tristique nulla aliquet enim. Habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper <b class='city'>$City </b> dignissim. Ante metus dictum at tempor commodo ullamcorper a lacus vestibulum. Tristique sollicitudin nibh sit amet commodo nulla facilisi. Arcu ac tortor dignissim convallis. <b class='city'>$City </b>. Integer enim neque volutpat ac tincidunt vitae semper quis.</div>
+        <div id='numbertest'>
+        <div class='numbers'> $number1  = $number2 ?</div>
+        <div>well...</div>
+        <br/>
+        $Boolean.";
+        if($number1 !== $number2){
+        echo "
+        <p id='falseEqual'> but, really
+        $Boolean2 </p>";  
+        // </div>";
+        }
+        echo "
+        </div>
+        </div>
+        </center>
+        </div>";
         ?>
     </body>
 </html>
