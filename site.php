98099f8ee8a7e72a8179cff62d137c707cea7009
amcmullen9291 <amcmullen9291@gmail.com>
2021-08-14 21:43:24 -0400

    learning CSS synatx

--- site.php
+++ site.php
@@ -1,20 +1,23 @@
 <!DOCTYPE html>
 <html>
     <head>
+    <link rel="stylesheet" type="text/css" href="style.css">
         <meta charset="uft-8">
     <title>My Page</title>
     </head>
     <body>
         <?php 
-        echo"<h1>My Heading</h1>";
-        echo"<table>
-        <thead>
+        echo"<center><h1>My Heading</h1></center>";
+        echo"<center><table>
+        <td class='title'></td>
+        <td class='title'>Team Stats</center></td>
+        <td class='title'></td>
+        </th>
         <tr>
         <td>Venue</td>
         <td>City</td>
         <td>Mascot</td>
         </tr>
-        </thead>
         <tbody>
         <tr>
         <td>Lambeau</td>
@@ -22,9 +25,16 @@
         <td>None</td>
         </tr>
         </tbody>
-        </table>";
+        </table></center>";
 
         echo "<hr/>";
+        echo"<br/><br/>
+        <div id='bottom_background'>
+        <center><div id='bottom'>
+        <div>Bottom Section</div>
+        <span id='line2'>(Strikingly similar to HTML)</span>
+        </div></center>
+        </div>"
 
         ?>
     </body>
